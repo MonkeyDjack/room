48f20ba3c6d560f2fd932af3d659a00a2873286f
Muhitdin <monkeydjack0@mail.ru>
2017-08-13 15:37:07 +0600

    пуп
    тест на телефоне

---
+++ js/core.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $('.menu-btn').click(function(){    
+      $('.menu').slideToggle(700); 
+      if ($(this).hasClass('not-active')) {
+        $(this).addClass('is-active').removeClass('not-active');
+      }else{
+        setTimeout(function(){
+          $('.is-active').addClass('not-active').removeClass('is-active')
+        },600)      
+      }   
+    });
+
+  $('.slider').slick({
+  arrows: false,
+  dots: false,
+  fade: true,
+  infinite: true,
+  speed: 500,
+  autoplay: true
+});
+
+});
