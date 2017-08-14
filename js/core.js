5528e5c8fc433d3caf15c1fa975c9197eb06fde2
Muhitdin <monkeydjack0@mail.ru>
2017-08-14 17:54:22 +0600

    фулл

--- js/core.js
+++ js/core.js
@@ -20,3 +20,32 @@ $(document).ready(function(){
 });
 
 });
+
+
+ 
+
+ 
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('#toTop').fadeIn();
+ 
+} else {
+ 
+$('#toTop').fadeOut();
+ 
+}
+ 
+});
+ 
+$('#toTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
+ 
